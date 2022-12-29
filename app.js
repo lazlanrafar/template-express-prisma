c4e539ca00949591ae94ebaa95a1a4ed51703c9d
lazlanrafar <lazlanrafar@gmail.com>
2022-12-29 16:45:33 +0700

    install and setup dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+require("dotenv").config();
 var app = express();
 
 app.use(logger("dev"));
