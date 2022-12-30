de1e1d0a26f622512ca7d28f545ac9a7c1cdd600
lazlanrafar <lazlanrafar@gmail.com>
2022-12-30 14:38:07 +0700

    add gitignore

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,6 +2,7 @@ const { Encrypt } = require("../../utils/hash-password");
 const { Ok, InternalServerError } = require("../../utils/http-response");
 const { EncryptToken } = require("../../utils/jwt");
 const { StoreUser, FetchUserByEmail } = require("../user/user.repository");
+const nodemailer = require("nodemailer");
 
 module.exports = {
   Register: async (req, res) => {
@@ -37,6 +38,32 @@ module.exports = {
       return Ok(res, payload, "User logged in successfully");
     } catch (error) {
       return InternalServerError(res, error, "Failed to login user");
+    }
+  },
+  SendEmail: async (req, res) => {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        service: process.env.EMAIL_SERVICE,
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: req.body.email,
+        subject: "Test Email",
+        text: "Hello World",
+      });
+
+      return Ok(res, "Email sent successfully");
+    } catch (error) {
+      console.log(error);
+      InternalServerError(res, error, "Failed to send email");
     }
   },
 };
--- app/auth/auth.route.js
+++ app/auth/auth.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { RegisterMiddleware, LoginMiddware } = require("./auth.middleware");
-const { Register, Login } = require("./auth.controller");
+const { Register, Login, SendEmail } = require("./auth.controller");
 const router = express.Router();
 
 router.post("/register", RegisterMiddleware, Register);
 router.post("/login", LoginMiddware, Login);
+router.post("/send-email", SendEmail);
 
 module.exports = router;
